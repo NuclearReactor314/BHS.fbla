e1197ddd77c0aed9c4c13225a4a9b5afd42c4fdf
NuclearReactor314 <145520137+NuclearReactor314@users.noreply.github.com>
2025-04-04 21:42:59 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Add smooth scroll to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Sticky navbar on scroll
+const navbar = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+});
